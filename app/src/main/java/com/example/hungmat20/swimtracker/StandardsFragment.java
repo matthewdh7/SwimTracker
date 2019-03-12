2a692205c423134f3b90fbef04efd5c9352075ed
matthewdh7 <matthewdh7@gmail.com>
2019-03-12 08:58:39 -0700

    3/12/19 school

--- app/src/main/java/com/example/hungmat20/swimtracker/StandardsFragment.java
+++ app/src/main/java/com/example/hungmat20/swimtracker/StandardsFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by hungmat20 on 3/6/2019.
@@ -19,4 +23,5 @@ public class StandardsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_standards, container, false);
     }
+
 }
