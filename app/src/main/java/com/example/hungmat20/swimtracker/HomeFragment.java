40e2f0e29d9d5cd23041ff86ed2964c44cb9f74c
matthewdh7 <matthewdh7@gmail.com>
2019-03-14 23:07:27 -0700

    fuk

--- app/src/main/java/com/example/hungmat20/swimtracker/HomeFragment.java
+++ app/src/main/java/com/example/hungmat20/swimtracker/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +18,11 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Test Line 1", "Test Line 2"));
-    }
 }
--- app/src/main/java/com/example/hungmat20/swimtracker/TimesFragment.java
+++ app/src/main/java/com/example/hungmat20/swimtracker/TimesFragment.java
@@ -1,22 +1,44 @@
 package com.example.hungmat20.swimtracker;
 
 import android.os.Bundle;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * Created by hungmat20 on 3/6/2019.
  */
 
 public class TimesFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_times, container, false);
+    }
+
+    public void onViewCreated(@Nullable Bundle savedInstanceState) {
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Test Line 1", "Test Line 2"));
+
+        mRecyclerView.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
